81d1328c235d826ad4fd9e63b8aae2d9f77e8be5
Wes McC <wkmccowin@yahoo.com>
2019-07-16 17:34:41 -0700

    Updates

---
+++ precache-manifest.3c9229026db132781c73f071f1a329c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "225aa25feaf2283a4d84b23b4ac9a265",
+    "url": "/nflpickem/index.html"
+  },
+  {
+    "revision": "f022e57fde4282e8566a",
+    "url": "/nflpickem/static/css/main.e07048e5.chunk.css"
+  },
+  {
+    "revision": "f35135349b7e039b7092",
+    "url": "/nflpickem/static/js/2.100c8e3f.chunk.js"
+  },
+  {
+    "revision": "f022e57fde4282e8566a",
+    "url": "/nflpickem/static/js/main.85649f52.chunk.js"
+  },
+  {
+    "revision": "c2ea8659931083ae1b78",
+    "url": "/nflpickem/static/js/runtime~main.db8c26dd.js"
+  }
+]);
